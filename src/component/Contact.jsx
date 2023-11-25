763c01b32eadaa8d91b3aeb985cac0f52f387e13
Vidhya0501 <vidhyasuri97@gmail.com>
2023-11-25 15:19:06 +0530

    contact form updated

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import github from '../assets/github1.png';
+import linkedin from '../assets/linkedin1.webp';
 function Contact() {
   return (
    <section id="contact">
         <h3>Contact me</h3>
         <div className="contact-form-div">
             <form className='contact-form'>
-                <input type="text" placeholder='Enter your Fullname'/>
-                <input type="email" placeholder='Enter your Email'/>
-                <textarea  name="message" rows="5" placeholder='Enter your message'/>
+                <input type="text" placeholder='Enter your Fullname' className='name'/>
+                <input type="email" placeholder='Enter your Email' className='email'/>
+                <textarea  name="message" rows="5" placeholder='Enter your message' className='msg'/>
                 <button type="submit" value="send" className='submit-btn'>Send</button>
             </form>
-
+            <div className="profile-links">
+              <img src={github} alt="Github" className='link'/>
+              <img src={linkedin} alt="Linkedin" className='link'/>
+            </div>
             
         </div>
        
